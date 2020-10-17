09a9d0f3156fa748e904fd226f50c0cef5769ba9
Arkady Muratov <muratov.arckady@yandex.ru>
2020-10-18 02:35:46 +0300

    Initial commit

---
+++ app/src/main/java/ru/arcadudu/mycalc/MainActivity.java
@@ -0,0 +1,171 @@
+package ru.arcadudu.mycalc;
+
+import android.os.Bundle;
+import android.text.SpannableStringBuilder;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.mariuszgromada.math.mxparser.Expression;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText display;
+    ImageButton ibBackspace;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        display = findViewById(R.id.et_display);
+        display.setShowSoftInputOnFocus(false); // remove keyboard appearance
+
+
+        // longPush for backspace
+        ibBackspace = findViewById(R.id.ib_backspace);
+        ibBackspace.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                display.setText("");
+                return true;
+            }
+        });
+
+
+    }
+
+    public void updateText(String newStr) {
+        String oldStr = display.getText().toString();
+        int cursorPos = display.getSelectionStart();
+        String leftStr = oldStr.substring(0, cursorPos);
+        String rightStr = oldStr.substring(cursorPos);
+        display.setText(String.format("%s%s%s", leftStr, newStr, rightStr));
+        display.setSelection(cursorPos + 1);
+    }
+
+    // backspace
+    public void backspaceIBtn(View view) {
+        int cursorPos = display.getSelectionStart();
+        int stringLen = display.getText().length();
+        if (cursorPos != 0 && stringLen != 0) {
+            SpannableStringBuilder selection = (SpannableStringBuilder) display.getText();
+            selection.replace(cursorPos - 1, cursorPos, "");
+            display.setText(selection);
+            display.setSelection(cursorPos - 1);
+        }
+    }
+
+    // num buttons
+    public void zeroBtn(View view) {
+        updateText("0");
+    }
+
+    public void oneBtn(View view) {
+        updateText("1");
+    }
+
+    public void twoBtn(View view) {
+        updateText("2");
+    }
+
+    public void threeBtn(View view) {
+        updateText("3");
+    }
+
+    public void fourBtn(View view) {
+        updateText("4");
+    }
+
+    public void fiveBtn(View view) {
+        updateText("5");
+    }
+
+    public void sixBtn(View view) {
+        updateText("6");
+    }
+
+    public void sevenBtn(View view) {
+        updateText("7");
+    }
+
+    public void eightBtn(View view) {
+        updateText("8");
+    }
+
+    public void nineBtn(View view) {
+        updateText("9");
+    }
+
+    // ops buttons
+    public void clearBtn(View view) {
+        display.setText("");
+    }
+
+    public void plusBtn(View view) {
+        updateText("+");
+    }
+
+    public void minusBtn(View view) {
+        updateText("-");
+    }
+
+    public void powerBtn(View view) {
+        updateText("^");
+    }
+
+    public void divideBtn(View view) {
+        updateText("÷");
+    }
+
+    public void multiplyBtn(View view) {
+        updateText("×");
+    }
+
+    public void parenthesesBtn(View view) {
+        int cursorPos = display.getSelectionStart();
+        int openPar = 0;
+        int closePar = 0;
+        int textLen = display.getText().length();
+        for (int i = 0; i < cursorPos; i++) {
+            if (display.getText().toString().substring(i, i + 1).equals("(")) {
+                openPar += 1;
+            }
+            if (display.getText().toString().substring(i, i + 1).equals(")")) {
+                closePar += 1;
+            }
+        }
+
+        if (openPar == closePar || display.getText().toString().substring(textLen - 1, textLen).equals("(")) {
+            updateText("(");
+        } else if (closePar < openPar && !display.getText().toString().substring(textLen - 1, textLen).equals("(")) {
+            updateText(")");
+        }
+        display.setSelection(cursorPos + 1);
+    }
+
+    public void percentBtn(View view) {
+        updateText("%");
+    }
+
+    public void dotBtn(View view) {
+        updateText(".");
+    }
+
+    public void equalsBtn(View view) {
+        String expression = display.getText().toString();
+        expression = expression.replaceAll("÷", "/");
+        expression = expression.replaceAll("×", "*");
+
+        Expression exp = new Expression(expression);
+        String result = String.valueOf(exp.calculate());
+
+        display.setText(result);
+        display.setSelection(result.length());
+
+    }
+
+
+}
